f473fe3831dde19bf86597be786de9d46256c189
Casper Thomsen <ct@clearhaus.com>
2018-05-04 16:43:47 +0200

    Accept some long lines

--- lib/pedicel-pay/backend.rb
+++ lib/pedicel-pay/backend.rb
@@ -235,32 +235,25 @@ module PedicelPay
     end
 
     def validate_ca
-      raise KeyError, 'ca private key not valid for ca certificate' unless
-        ca_certificate.check_private_key(ca_key)
+      raise KeyError, 'ca private key not valid for ca certificate' unless ca_certificate.check_private_key(ca_key)
 
-      raise CertificateError, 'ca certificate is not self-signed' unless
-        ca_certificate.verify(ca_key)
+      raise CertificateError, 'ca certificate is not self-signed' unless ca_certificate.verify(ca_key)
 
       true
     end
 
     def validate_intermediate
-      raise KeyError, 'intermediate private key not valid for intermediate '\
-        'certificate' unless
-        intermediate_certificate.check_private_key(intermediate_key)
+      raise KeyError, 'intermediate private key not valid for intermediate certificate' unless intermediate_certificate.check_private_key(intermediate_key)
 
-      raise CertificateError, 'intermediate certificate not signed by ca' unless
-        intermediate_certificate.verify(ca_key)
+      raise CertificateError, 'intermediate certificate not signed by ca' unless intermediate_certificate.verify(ca_key)
 
       true
     end
 
     def validate_leaf
-      raise KeyError, 'leaf private key not valid for leaf certificate' unless
-        leaf_certificate.check_private_key(leaf_key)
+      raise KeyError, 'leaf private key not valid for leaf certificate' unless leaf_certificate.check_private_key(leaf_key)
 
-      raise CertificateError, 'leaf certificate not signed by intermediate' \
-        unless leaf_certificate.verify(intermediate_key)
+      raise CertificateError, 'leaf certificate not signed by intermediate' unless leaf_certificate.verify(intermediate_key)
 
       true
     end
